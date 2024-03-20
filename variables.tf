80d56b3ddfc730b5701bc0a284f7f7ffc796aec7
Om Solanki <om@Oms-Laptop.local>
2024-03-20 15:39:57 -0400

    Added terraform fmt changes

--- variables.tf
+++ variables.tf
@@ -257,12 +257,12 @@ variable "google_service_account_account_id" {
 variable "google_service_account_display_name" {
   default = "Service Account Logging"
 }
-variable "google_service_account_description" { 
+variable "google_service_account_description" {
   default = "Created by Terraform for Logging"
 }
 variable "google_project_iam_binding_logging_admin" {
   default = "roles/logging.admin"
 }
-variable "google_project_iam_binding_monitoring_metric_writer" { 
+variable "google_project_iam_binding_monitoring_metric_writer" {
   default = "roles/monitoring.metricWriter"
 }
